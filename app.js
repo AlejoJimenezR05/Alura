6693412efaddf4f9edaea2afe0447219059fb791
AlejoJimenezR05 <alejojimenezr05@gmail.com>
2025-03-17 18:42:58 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+let Amigos = []; //* Crear lista para almacenar amigos  *\\
+let AmigosSorteados = []; //* Esta lista almacenal los números  *\\
+
+const regex = /^(?!\s*$)[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/;
+  //* Esto nos permite restringir a que el nombre del usuario tenga solo letras y espacios *\\
+
+  function MostrarAmigos(elemento, texto) {
+let elementoHTML = document.querySelector(elemento);
+elementoHTML.innerHTML = texto;
+return;
+
+  }
+
+  console.log ("Vamos a empezar")
+function agregarAmigo() {
+
+}
